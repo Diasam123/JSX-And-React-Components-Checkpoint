9579437592d6f8ad308f7c525f7feb755f0af6be
Diasam123 <126729750+Diasam123@users.noreply.github.com>
2023-08-02 18:08:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Name from "./components/Name";
+import Price from "./components/Price";
+import Description from "./components/Description";
+import Image from "./components/Image";
+import { productData } from "./product";
 
-function App() {
+const firstName = "Dairo"; 
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello, {firstName ? firstName : "there"}!</h1>
+      {firstName && <Image image={productData.image} />}
+      <div className="card">
+        <div className="card-body">
+          <Name name={productData.name} />
+          <Price price={productData.price} />
+          <Description description={productData.description} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
